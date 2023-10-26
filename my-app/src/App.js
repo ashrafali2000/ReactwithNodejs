49df386274bf554e6110a06c250bc585a6928615
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-26 12:57:54 +0500

    add routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
+import Layout from './components/layout';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Routes>
+
+    <Route path='/' element={<Layout></Layout>}>
+     <Route index element={<Home></Home>}></Route>
+     
+
+    </Route>
+
+
+      </Routes>
     </div>
   );
 }
