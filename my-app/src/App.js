58a112d766134458c15ef363fd20fd3008aeb1b6
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-10-26 16:27:31 +0500

    add routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/home';
 import Layout from './components/layout';
+import Products from './pages/products';
+import SignIn from './pages/signIn';
+import SignUp from './pages/signUp';
 
 function App() {
   return (
     <div className="App">
       <Routes>
 
-    <Route path='/' element={<Layout></Layout>}>
+    <Route path="/" element={<Layout></Layout>}>
      <Route index element={<Home></Home>}></Route>
+     <Route path='/products' element={<Products></Products>}></Route>
+     <Route path='/signin' element={<SignIn></SignIn>}></Route>
+     <Route path='/signup' element={<SignUp></SignUp>}></Route>
      
 
     </Route>
